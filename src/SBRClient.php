b3ecb9b15c4557a884b3ec1e49a03b7a3f72271b
AdvancedMaXX <advancedmaxx@gmail.com>
2017-11-08 21:34:46 +0300

    Init

--- src/SBRClient.php
+++ src/SBRClient.php
@@ -6,13 +6,53 @@ namespace rstmpw\sbrpgate;
 
 class SBRClient
 {
-    public function __construct(string $DSNString)
-    {
+    protected $login = null;
+    protected $password = null;
+    protected $initOpts = [
+        'endpoints' => [
+            'newOrder' => 'https://3dsec.sberbank.ru/payment/rest/register.do',
+            //'newOrder' => 'https://3dsec.sberbank.ru/payment/rest/registerPreAuth.do',
+            'orderStatus' => 'https://3dsec.sberbank.ru/payment/rest/getOrderStatus.do',
+            //'orderStatus' => 'https://3dsec.sberbank.ru/payment/rest/getOrderStatusExtended.do'
+        ]
+    ];
 
+    public function __construct(string $login, string $password, array $initOpts=null)
+    {
+        $this->login = $login;
+        $this->password = $password;
+        if(!is_null($initOpts)) $this->initOpts = $initOpts;
     }
 
-    public static function newOrder(string &$data): array
+    public function newOrder(string $orderNum, int $amount, string $returnURL, string $description = null, string $failUrl = null): array
     {
-        return [];
+        $return = [
+            'orderId' => null,
+            'redirectUrl' => null
+        ];
+
+        $requestData = [
+            'userName',
+            'password',
+            'orderNumber',
+            'amount',
+            'description',
+            'returnUrl',
+            'failUrl'
+        ];
+
+        return $return;
     }
+
+    public function orderStatus(string $orderId):bool {
+        $requestData = [
+            'userName',
+            'password',
+            'orderId'
+        ];
+
+        return true;
+    }
+
+
 }
